c3d6cabc688ec3c2cd72c721c1c6ef9a414e1b15
nedduc <marinacudden@gmail.com>
2021-10-28 15:40:04 +0000

    failed

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -2,17 +2,17 @@
  * @jest-environment jsdom
  */
 
- const buttonClick = require("../button");
+const buttonClick = require("../button");
 
- beforeAll(() => {
+beforeAll(() => {
      let fs = require("fs");
      let fileContents = fs.readFileSync("index.html", "utf-8");
-     document.open(); //open the file
-     document.write(fileContents); //write file contents
-     document.close(); //then close it
- });
+     document.open();
+     document.write(fileContents);
+     document.close();
+});
  
- describe("DOM tests", () => {
+describe("DOM tests", () => {
      test("Expects content to change", () => {
          buttonClick();
          expect(document.getElementById("par")
@@ -21,4 +21,4 @@
      test("h1 should exist", () => {
          expect(document.getElementsByTagName("h1").length).toBe(1);
      });
- });
+});
